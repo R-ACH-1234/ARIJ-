682f897aaf8ea1a9c8e23d78a173628a1e75a288
R-ACH-1234 <145674744+R-ACH-1234@users.noreply.github.com>
2026-05-09 15:35:57 +0000

    feat: Update app icons to external CDN
    Replaces local app icon assets with a publicly accessible CDN link. This simplifies asset management and reduces bundle size.

--- fix_urls.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let html = fs.readFileSync('index.html', 'utf8');
-html = html.replace(/https:\/\/f\.top4top\.io\/s_3781y3o7d0\.png/g, '/logo.png');
-fs.writeFileSync('index.html', html);
-
-let manifest = fs.readFileSync('public/manifest.json', 'utf8');
-manifest = manifest.replace(/https:\/\/f\.top4top\.io\/s_3781y3o7d0\.png/g, '/logo.png');
-fs.writeFileSync('public/manifest.json', manifest);
